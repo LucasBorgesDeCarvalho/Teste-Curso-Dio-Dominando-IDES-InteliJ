5dba687ae1d58b033cc493ab2edfa7f85adbb14a
LucasBorgesDeCarvalho <lucas.carvalho30111@gmail.com>
2022-05-21 13:58:59 -0300

    Criando class Livro

--- src/br/com/dio/PrimeiroProjeto.java
+++ src/br/com/dio/PrimeiroProjeto.java
@@ -7,6 +7,42 @@ public class PrimeiroProjeto {
 
         Gato gato = new Gato();
         System.out.println(gato);
+
+        livro livro1 = new livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
+    }
+}
+
+class livro{
+    private String nome;
+    private Integer numPagnias;
+
+    public livro(String nome, Integer numPagnias) {
+        this.nome = nome;
+        this.numPagnias = numPagnias;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPagnias() {
+        return numPagnias;
+    }
+
+    public void setNumPagnias(Integer numPagnias) {
+        this.numPagnias = numPagnias;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPagnias=" + numPagnias +
+                '}';
+    }
 }
